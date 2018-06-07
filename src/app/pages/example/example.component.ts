aeed78881ecbaffac80d4381102559bc68334981
ard333 <ard333.ardiansyah@gmail.com>
2018-06-07 15:36:16 +0700

    add more example compoenent

--- src/app/pages/example/example.component.ts
+++ src/app/pages/example/example.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
 
 @Component({
   selector: 'app-example',
@@ -7,9 +16,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExampleComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns = ['position', 'name', 'weight', 'symbol', 'action'];
+  dataSource = new MatTableDataSource<PeriodicElement>([]);
+
+  isLoadingResults;
+  resultsLength;
+  currentPage;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  
+  handlePage(e: any) {
+    this.currentPage = e.pageIndex;
+    this.getPage(this.currentPage);
+  }
+
+  getPage(page: number) {
+    this.isLoadingResults = true;
+    //simulation
+    this.delay(1000).then(() => {
+      this.dataSource = new MatTableDataSource<PeriodicElement>(this.getData(this.currentPage));
+      this.isLoadingResults = false;
+      this.resultsLength = 100;
+    });
+  }
 
   ngOnInit() {
+    this.currentPage = 0;
+    this.dataSource.paginator = this.paginator;
+    this.getPage(this.currentPage);
+  }
+
+  //just shuffle
+  getData(page: number): Array<PeriodicElement> {
+    var dataSource = [
+      { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+      { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+      { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+      { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+      { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+      { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+      { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+      { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+      { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+      { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+    ];
+    var currentIndex = dataSource.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = dataSource[currentIndex];
+      dataSource[currentIndex] = dataSource[randomIndex];
+      dataSource[randomIndex] = temporaryValue;
+    }
+    return dataSource;
+  }
+
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
   }
 
 }
--- src/app/pages/example/example.module.ts
+++ src/app/pages/example/example.module.ts
@@ -2,9 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExampleComponent } from "./example.component";
 
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+
+    MatCardModule,
+    MatDividerModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
   ],
   declarations: [
     ExampleComponent
